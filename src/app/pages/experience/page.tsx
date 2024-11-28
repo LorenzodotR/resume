ebeeb49665c09b5d7c7bc3ea952fe81bdb222522
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2024-11-28 11:48:59 -0300

    text changes

--- src/app/pages/experience/page.tsx
+++ src/app/pages/experience/page.tsx
@@ -21,23 +21,29 @@ export default function About() {
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4">
                     <div className="min-h-[50vh] flex-1 rounded-xl bg-stone-600 border md:min-h-min">
-                        <p className="p-4 flex flex-row justify-center">
-                            EXPERIÊNCIAS EM ESTÁGIOS OU PROJETOS RELEVANTES
-                        </p>
+                        <p className="p-4 flex flex-row justify-center">EXPERIÊNCIAS EM PROJETOS RELEVANTES</p>
                         <div className="p-4 grid auto-rows-min gap-4 md:grid-cols-2">
                             <div className="aspect-video rounded-xl bg-muted/50 border">
-                                <p className="p-4 flex flex-row justify-center">
-                                    Software Developer | Educação Financeira - Belo Horizonte/MG
+                                <p className="p-2 flex flex-row justify-center">
+                                    Software Developer | Projeto de Desenvolvimento App Educação Financeira - Belo Horizonte/MG
                                 </p>
-                                <p className="p-4">
-                                    Educação Financeira é um projeto de idealização pessoal onde eu estou sendo o
+                                <p className="flex flex-row justify-center">Em Progresso</p>
+
+                                <p className="p-2">
+                                    O App de Educação Financeira é um projeto de idealização pessoal onde eu estou sendo o
                                     responsável pelo levantamento de requisitos, estruturação do projeto e
                                     desenvolvimento.
                                 </p>
-                                <p className="p-4">
-                                    Tecnologias: NodeJs, Express, React, Jenkins, Oracle Cloud, Docker, Portainer, Nginx
+                                <p className="p-2">
+                                    <span>
+                                        Tecnologias: &nbsp;
+                                        <strong>
+                                            NodeJs, Express, React, Jenkins, Oracle Cloud, Docker, Portainer, Nginx
+                                            Proxy Manager
+                                        </strong>
+                                    </span>
                                 </p>
-                                <ul className="p-4">
+                                <ul className="p-2">
                                     <li>
                                         Equilíbrio é um projeto idealizado por mim e que está em seu momento inicial,
                                         após algumas rodadas de apresentação, estou entrando com ele como Projeto de
@@ -52,19 +58,26 @@ export default function About() {
                                 </ul>
                             </div>
                             <div className="aspect-video rounded-xl bg-muted/50 border">
-                                <p className="p-4 flex flex-row justify-center">
+                                <p className="p-2 flex flex-row justify-center">
                                     Software Developer | Programa Incluir - Belo Horizonte/MG
                                 </p>
-                                <p className="p-4">
+                                <p className="flex flex-row justify-center">Julho 2022 - Atualmente</p>
+
+                                <p className="p-2">
                                     O Incluir é um programa social dentro do prédio de Engenharia da UFMG, destinado a
                                     ensinar inglês e outras disciplinas como empreendedorismo e educação financeira,
                                     para a população interessada e com emissão de certificado.
                                 </p>
-                                <p className="p-4">
-                                    Tecnologias: NodeJs, Express, React, Jenkins, Oracle Cloud, Docker, Portainer, Nginx
-                                    Proxy Manager
+                                <p className="p-2">
+                                    <span>
+                                        Tecnologias: &nbsp;
+                                        <strong>
+                                            NodeJs, Express, React, Jenkins, Oracle Cloud, Docker, Portainer, Nginx
+                                            Proxy Manager
+                                        </strong>
+                                    </span>
                                 </p>
-                                <ul className="p-4">
+                                <ul className="p-2">
                                     <li>Professor voluntário de Inglês</li>
                                     <br />
                                     <li>Desenvolvedor de Software</li>
@@ -76,18 +89,48 @@ export default function About() {
                         <p className="p-4 flex flex-row justify-center">EXPERIÊNCIAS PROFISSIONAIS</p>
                         <div className="p-4 grid auto-rows-min gap-4 md:grid-cols-2">
                             <div className="aspect-video rounded-xl bg-muted/50 border">
-                                <p className="p-4 flex flex-row justify-center">
+                                <p className="p-2 flex flex-row justify-center">
+                                    Software Developer | Greg (ex-ContSelf) - Belo Horizonte/MG
+                                </p>
+                                <p className="flex flex-row justify-center">Março 2022 - Julho 2022</p>
+                                <p className="p-2">
+                                    Dentro da Greg atuei como Estagiário FullStack, porém com alguns escopos de projetos
+                                    e atividades muito significativas no que diz respeito a impacto para com o cliente,
+                                    principalmente na parte de pagamentos via boletos, e atendimento via ChatBot .
+                                </p>
+                                <p className="p-2">Tecnologias: C#, ASP.NET MVC, SQL Server, JavaScript.</p>
+                                <ul className="p-2">
+                                    <li>
+                                        Fui responsável pela criação de um ChatBot de atendimento ao cliente, que dentro
+                                        da primeira semana teve um feedback positivo de 100% dos clientes.
+                                    </li>
+                                    <br />
+                                    <li>
+                                        Fui responsável pela criação de um sistema gerador de boletos de pagamentos,
+                                        para pagamentos de carnê.
+                                    </li>
+                                    <br />
+                                    <li>
+                                        Atuei no contato direto com o cliente e na manutenção do sistema integrador com
+                                        as prefeituras para geração de nota fiscal de recebimento.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="aspect-video rounded-xl bg-muted/50 border">
+                                <p className="p-2 flex flex-row justify-center">
                                     Software Developer | Atomos Hyla - Belo Horizonte/MG
                                 </p>
-                                <p className="p-4">
+                                <p className="flex flex-row justify-center">Setembro 2022 - Atualmente</p>
+
+                                <p className="p-2">
                                     Dentro da Atomos atuo como desenvolvedor fullstack e fui responsável por realizar
                                     muitas tarefas importantes, acredito que tive bastante impacto em projetos grandes e
                                     com bons feedbacks.
                                 </p>
-                                <p className="p-4">
+                                <p className="p-2">
                                     Tecnologias: C#, Angular 16, SQL Server, TypeScript, Oracle DataBase, AzureDevOps.
                                 </p>
-                                <ul className="p-4">
+                                <ul className="p-2">
                                     <li>
                                         Fui responsável pela criação de um projeto específico para a empresa AIRBUS,
                                         onde eu fui o levantador de requisitos e o executor do projeto, onde tive um
@@ -104,33 +147,6 @@ export default function About() {
                                         Responsável pela criação de telas para visualização de gráficos importantes para
                                         a empresa Anglo American, onde era possível gerenciar os riscos de suas
                                         barragens com essas informações.
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="aspect-video rounded-xl bg-muted/50 border">
-                                <p className="p-4 flex flex-row justify-center">
-                                    Software Developer | Greg (ex-ContSelf) - Belo Horizonte/MG
-                                </p>
-                                <p className="p-4">
-                                    Dentro da Greg atuei como Estagiário FullStack, porém com alguns escopos de projetos
-                                    e atividades muito significativas no que diz respeito a impacto para com o cliente,
-                                    principalmente na parte de pagamentos via boletos, e atendimento via ChatBot .
-                                </p>
-                                <p className="p-4">Tecnologias: C#, ASP.NET MVC, SQL Server, JavaScript.</p>
-                                <ul className="p-4">
-                                    <li>
-                                        Fui responsável pela criação de um ChatBot de atendimento ao cliente, que dentro
-                                        da primeira semana teve um feedback positivo de 100% dos clientes.
-                                    </li>
-                                    <br />
-                                    <li>
-                                        Fui responsável pela criação de um sistema gerador de boletos de pagamentos,
-                                        para pagamentos de carnê.
-                                    </li>
-                                    <br />
-                                    <li>
-                                        Atuei no contato direto com o cliente e na manutenção do sistema integrador com
-                                        as prefeituras para geração de nota fiscal de recebimento.
                                     </li>
                                 </ul>
                             </div>
